ce94e9431b000b7390e4aa9827639d3d997b486d
Niopthen <niopthen@eymail.de>
2017-03-31 13:52:49 +0200

    create Sync Direction

---
+++ dbSync.conf.template.php
@@ -0,0 +1,29 @@
+<?php
+
+###################################################################
+# DB Sync Script Configuration Template
+###################################################################
+
+/* GLOBAL Configuration Data for the Sync Process */
+$mySQL['GLOBAL']['SYNC_ALL'] = 0;
+$mySQL['GLOBAL']['SYNC_DIRECTION'] = 1; # 1 => ONE2TWO | 2 TWO2ONE
+
+/* 
+ * Database Information 
+ */
+
+
+/* Server ONE */
+$mySQL['ONE']['SERVER']     = "";
+$mySQL['ONE']['USER']       = "";
+$mySQL['ONE']['PASSWORD']   = "";
+$mySQL['ONE']['DATABASE']   = "";
+$mySQL['ONE']['TABLE']      = "";
+
+
+/* Server TWO */
+$mySQL['TWO']['SERVER']     = "";
+$mySQL['TWO']['USER']       = "";
+$mySQL['TWO']['PASSWORD']   = "";
+$mySQL['TWO']['DATABASE']   = "";
+$mySQL['TWO']['TABLE']      = "";
