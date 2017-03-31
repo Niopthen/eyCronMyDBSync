aeb3a41d5ea90a003c1d488fb08bab70066c43a1
Niopthen <niopthen@eymail.de>
2017-03-31 14:07:59 +0200

    .Update

--- dbSync.php
+++ dbSync.php
@@ -7,3 +7,40 @@ if (!file_exists('dbSync.conf.php'))
 {
     echo 'the Sync file is missing /n';
 }
+else
+{
+    require_once './dbSync.conf.php';
+}
+
+$myDBSync = new myDBSync($mySQL);
+$myDBSync->mySQLSync($mySQL['GLOBAL']['SYNC_DIRECTION']);
+
+
+
+class myDBSync
+{
+    protected $mySQLConOne;
+    protected $mySQLConTwo;
+    
+    function __construct() 
+    {
+    
+    }
+   
+    
+    function mySQLSync($SyncType=1)
+    {
+        
+    }
+    
+    function mysqlConnect()
+    {
+        
+    }
+    
+    
+    
+    
+    
+    
+}
