778f3aaa7f381ae050dc4c8d79c55e938804b563
Niopthen <niopthen@eymail.de>
2017-04-03 15:25:20 +0200

    DB Connection

--- dbSync.conf.template.php
+++ dbSync.conf.template.php
@@ -7,6 +7,10 @@
 /* GLOBAL Configuration Data for the Sync Process */
 $mySQL['GLOBAL']['SYNC_ALL'] = 0;
 $mySQL['GLOBAL']['SYNC_DIRECTION'] = 1; # 1 => ONE2TWO | 2 TWO2ONE
+$mySQL['GLOBAL']['MAX_NUMROWS'] = 20;
+$mySQL['GLOBAL']['SYNC_ID'] = 'row_id';
+
+
 
 /* 
  * Database Information 
@@ -14,16 +18,20 @@ $mySQL['GLOBAL']['SYNC_DIRECTION'] = 1; # 1 => ONE2TWO | 2 TWO2ONE
 
 
 /* Server ONE */
-$mySQL['ONE']['SERVER']     = "";
-$mySQL['ONE']['USER']       = "";
-$mySQL['ONE']['PASSWORD']   = "";
-$mySQL['ONE']['DATABASE']   = "";
-$mySQL['ONE']['TABLE']      = "";
+$mySQL['ONE']['SERVER']     = '';
+$mySQL['ONE']['USER']       = '';
+$mySQL['ONE']['PASSWORD']   = '';
+$mySQL['ONE']['DATABASE']   = '';
+$mySQL['ONE']['TABLE']      = '';
+$mySQL['ONE']['PORT']       = '';
+$mySQL['ONE']['CHARSET']    = '';
 
 
 /* Server TWO */
-$mySQL['TWO']['SERVER']     = "";
-$mySQL['TWO']['USER']       = "";
-$mySQL['TWO']['PASSWORD']   = "";
-$mySQL['TWO']['DATABASE']   = "";
-$mySQL['TWO']['TABLE']      = "";
+$mySQL['TWO']['SERVER']     = '';
+$mySQL['TWO']['USER']       = '';
+$mySQL['TWO']['PASSWORD']   = '';
+$mySQL['TWO']['DATABASE']   = '';
+$mySQL['TWO']['TABLE']      = '';
+$mySQL['TWO']['PORT']       = '';
+$mySQL['TWO']['CHARSET']    = '';
--- dbSync.php
+++ dbSync.php
@@ -1,4 +1,5 @@
 <?php
+
 ###################################################################
 # DB Sync Script
 ###################################################################
@@ -15,32 +16,62 @@ else
 $myDBSync = new myDBSync($mySQL);
 $myDBSync->mySQLSync($mySQL['GLOBAL']['SYNC_DIRECTION']);
 
-
-
 class myDBSync
 {
-    protected $mySQLConOne;
-    protected $mySQLConTwo;
-    
-    function __construct() 
+
+    private $mySQLConOne;
+    private $mySQLConTwo;
+    private $mySQLArray;
+
+    function __construct($mySQL)
     {
-    
+        $this->mySQLArray = $mySQL;
+        /* make the connection to DB One and DB Two */
+        $this->mySQLConOne = $this->mysqliConnect($this->mySQLArray['ONE']);
+        $this->mySQLConTwo = $this->mysqliConnect($this->mySQLArray['TWO']);
+        
+        // make sync
+        $this->mySQLSync();
+          
     }
-   
-    
-    function mySQLSync($SyncType=1)
+
+    private function mySQLSync()
+    {
+        
+        
+    }
+
+/**
+ * Connection to the databases from the config file
+ * @param array $mySQLCon
+ * @return string
+ */
+    private function mysqliConnect($mySQLCon)
+    {
+        $dbh = mysqli_init();
+        if (!$dbh)
+        {
+            die('Connection for ' . $mySQLCon['SERVER'] . ' is not possible');
+        }
+
+        mysqli_connect($mySQLCon['SERVER'], $mySQLCon['USER'], $mySQLCon['PASSWORD'], $mySQLCon['DATABASE'], $mySQLCon['PORT'], NULL, MYSQLI_CLIENT_COMPRESS);
+        mysqli_set_charset($dbh, $mySQLCon['CHARSET']);
+        return $dbh;
+    }
+
+    private function mySQL_Select()
     {
         
     }
-    
-    function mysqlConnect()
+
+    private function mySQL_Insert()
     {
         
     }
-    
-    
-    
-    
-    
-    
+
+    function __destruct()
+    {
+        
+    }
+
 }
