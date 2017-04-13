ff8af07646beff57d289ea4b3c5f5c01196acf77
Niopthen <niopthen@eymail.de>
2017-04-13 10:28:07 +0200

    Anpassungen und Strukturänderungen

--- dbSync.php
+++ dbSync.php
@@ -4,92 +4,125 @@
 # DB Sync Script
 ###################################################################
 
+/* Load the Config File - if not available Stop the Script */
 if (!file_exists('dbSync.conf.php'))
 {
-    echo 'the Sync Config File is missing /n';
+    die('the Sync Config File is missing /n');
 }
 else
 {
     require_once './dbSync.conf.php';
 }
+$WriteLog = $DBSyncConf['DEFAULT']['WRITE_LOG'];
+$SynTyp = $DBSyncConf['DEFAULT']['SYNC_TYP'];
 
-$myDBSync = new myDBSync($mySQL);
-$myDBSync->mySQLSync($mySQL['GLOBAL']['SYNC_DIRECTION']);
 
-class myDBSync
+
+
+/* Check the SyncTyp and assign it
+ * 0 - Total Sync from DB One to the DB Two (only possible if DB Two is emty)
+ * 1 - Sync DB One to the DB Two and check the primary key if it is already in DB Two take the next entry
+ * 2 - Like SyncTyp 1 but if the primary Key is in DB Two then delete this entry and write the entry form DB One
+ * 
+ * Check if we should write a log
+ */
+
+if (is_array($_GET))
+{
+    if ((key_exists('SyncTyp', $_GET)) && (in_array($_GET['SyncTyp'], $DBSyncConf['GLOBAL']['AvailSyncTyp'])))
+    {
+        $SyncTyp = $_GET['SyncTyp'];
+    }
+
+    if ((key_exists('WriteLog', $_GET)) && (in_array($_GET['WriteLog'], $DBSyncConf['GLOBAL']['WriteLog'])))
+    {
+        $WriteLog = $_GET['WriteLog'];
+    }
+}
+
+
+
+$DBSync = new DBSync($DBSyncConf, $SyncTyp, $WriteLog);
+$DBSync->mySQLSync($DBSyncConf['GLOBAL']['SYNC_DIRECTION']);
+
+class DBSync
 {
 
-    private $mySQLConOne;
-    private $mySQLConTwo;
-    private $mySQLArray;
-    private $tmp_count;
-    private $tmp_allrows;
+    private $SQLConOne;
+    private $SQLConTwo;
+    private $DBSyncConf;
+    private $SyncTyp;
+    private $tmp_rows_db_one;
+    private $tmp_rows_db_two;
 
-    function __construct($mySQL)
+    function __construct($DBSyncConf, $SyncTyp = 1, $WriteLog = 0)
     {
-        $this->mySQLArray = $mySQL;
-        /* make the connection to DB One and DB Two */
-        $this->mySQLConOne = $this->mysqliConnect($this->mySQLArray['ONE']);
-        $this->mySQLConTwo = $this->mysqliConnect($this->mySQLArray['TWO']);
+        $this->SyncTyp = $SyncTyp;
+        $this->DBSyncConf = $DBSyncConf;
+
+        // connect to DB One and DB Two
+        $this->SQLConOne = $this->Connect($this->DBSyncConf['ONE']);
+        $this->SQLConTwo = $this->Connect($this->DBSyncConf['TWO']);
 
         // make sync
-        $this->mySQLSync();
+        $this->SQLSync();
     }
 
-
-    private function mySQLSync()
+    private function SQLSync()
     {
-        
-        
-        // zaehle mir die anzahl der reihen der ersten DB und hole mir die erste anzahl lt. config
-        // 
-        // hole mir SELECT * FROM tbl LIMIT 5,10;  # Retrieve rows 6-15
+        /*
+         * Structure what to code 
+         * a. count the rows in db one and db two
+         * b. 
+         * 
+         * 
+         * e.g. SELECT * FROM tbl LIMIT 5,10 # Retrieve rows 6-15
+         * 
+         * 
+         */
     }
-
 
     /**
      * Connection to the databases from the config file
-     * @param array $mySQLCon
+     * @param array $SQLCon
      * @return string
      */
-    private function mysqliConnect($mySQLCon)
+    private function Connect($SQLCon)
     {
         $dbh = mysqli_init();
         if (!$dbh)
         {
-            die('Connection for ' . $mySQLCon['SERVER'] . ' is not possible');
+            die('Connection for ' . $SQLCon['SERVER'] . ' is not possible');
         }
 
-        mysqli_connect($mySQLCon['SERVER'], $mySQLCon['USER'], $mySQLCon['PASSWORD'], $mySQLCon['DATABASE'], $mySQLCon['PORT'], NULL, MYSQLI_CLIENT_COMPRESS);
-        mysqli_set_charset($dbh, $mySQLCon['CHARSET']);
+        mysqli_connect($SQLCon['SERVER'], $SQLCon['USER'], $SQLCon['PASSWORD'], $SQLCon['DATABASE'], $SQLCon['PORT'], NULL, MYSQLI_CLIENT_COMPRESS);
+        mysqli_set_charset($dbh, $SQLCon['CHARSET']);
         return $dbh;
     }
 
-
-    private function mySQL_Select()
-    {
-        
-        
-        
-    }
-
-
-    private function mySQL_Insert()
+    private function Select()
     {
         
     }
 
-
-    private function mySQL_Delete()
+    private function Insert()
     {
         
     }
 
+    private function Delete()
+    {
+        
+    }
+
+    private function Write_Log($message)
+    {
+        
+    }
 
     function __destruct()
     {
         
     }
-
 
 }
--- library/dbSync.conf.template.php
+++ library/dbSync.conf.template.php
@@ -5,33 +5,37 @@
 ###################################################################
 
 /* GLOBAL Configuration Data for the Sync Process */
-$mySQL['GLOBAL']['SYNC_ALL'] = 0;
-$mySQL['GLOBAL']['SYNC_DIRECTION'] = 1; # 1 => ONE2TWO | 2 TWO2ONE
-$mySQL['GLOBAL']['MAX_NUMROWS'] = 20;
-$mySQL['GLOBAL']['SYNC_ID'] = 'row_id';
-$mySQL['GLOBAL']['LOG']['FILE'] = '';
+$DBSyncConf['GLOBAL']['SYNC_ALL'] = 0;
+$DBSyncConf['GLOBAL']['SYNC_DIRECTION'] = 1; # 1 => ONE2TWO | 2 TWO2ONE
+$DBSyncConf['GLOBAL']['MAX_NUMROWS'] = 20;
+$DBSyncConf['GLOBAL']['SYNC_ID'] = 'row_id';
+$DBSyncConf['GLOBAL']['LOG']['FILE'] = '/var/logs/DBSyncLog.log';
+$DBSyncConf['GLOBAL']['AVAIL_SYNC_TYP'] = array(0, 1, 2);
+$DBSyncConf['GLOBAL']['WRITE_LOG'] = array(0, 1);
 
+$DBSyncConf['DEFAULT']['WRITE_LOG'] = 0;
+$DBSyncConf['DEFAULT']['SYNC_TYP'] = 1;
 
-/* 
+/*
  * Database Information 
  */
 
 
 /* Server ONE */
-$mySQL['ONE']['SERVER']     = '';
-$mySQL['ONE']['USER']       = '';
-$mySQL['ONE']['PASSWORD']   = '';
-$mySQL['ONE']['DATABASE']   = '';
-$mySQL['ONE']['TABLE']      = '';
-$mySQL['ONE']['PORT']       = '';
-$mySQL['ONE']['CHARSET']    = '';
+$DBSyncConf['ONE']['SERVER'] = '';
+$DBSyncConf['ONE']['USER'] = '';
+$DBSyncConf['ONE']['PASSWORD'] = '';
+$DBSyncConf['ONE']['DATABASE'] = '';
+$DBSyncConf['ONE']['TABLE'] = '';
+$DBSyncConf['ONE']['PORT'] = '';
+$DBSyncConf['ONE']['CHARSET'] = '';
 
 
 /* Server TWO */
-$mySQL['TWO']['SERVER']     = '';
-$mySQL['TWO']['USER']       = '';
-$mySQL['TWO']['PASSWORD']   = '';
-$mySQL['TWO']['DATABASE']   = '';
-$mySQL['TWO']['TABLE']      = '';
-$mySQL['TWO']['PORT']       = '';
-$mySQL['TWO']['CHARSET']    = '';
+$DBSyncConf['TWO']['SERVER'] = '';
+$DBSyncConf['TWO']['USER'] = '';
+$DBSyncConf['TWO']['PASSWORD'] = '';
+$DBSyncConf['TWO']['DATABASE'] = '';
+$DBSyncConf['TWO']['TABLE'] = '';
+$DBSyncConf['TWO']['PORT'] = '';
+$DBSyncConf['TWO']['CHARSET'] = '';
